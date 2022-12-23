c31404742c9b8683b23257676daa31c0be7581d9
yaelvr <yaelvr200007@gmail.com>
2022-12-23 03:58:02 -0600

    Producto-Detail.html url,views y sus static files(css js) añadidos

--- store/store/urls.py
+++ store/store/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from PrincipalPage.views import Home
+from PrincipalPage.views import Home, Producto
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',Home),
+    path('1',Producto),
 ]
