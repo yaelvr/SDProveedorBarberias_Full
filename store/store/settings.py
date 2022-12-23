356dc664e895c754c3bd29c3084493fa6debc577
yaelvr <yaelvr200007gmail.com>
2022-12-23 02:35:16 -0600

    Store settings DATABASES modificado

--- store/store/settings.py
+++ store/store/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'store.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'store_db',
+       'USER': 'postgres',
+       'PASSWORD': 'admin',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
